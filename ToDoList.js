3dad22f5dca3db82d4b2abe1340c90ed013bf258
Sushruta Gurumurthy Nadiger <sushrutagn@gmail.com>
2016-08-22 10:39:00 +0530

    Add files via upload
    To-Do List App

---
+++ ToDoList.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('listapp', []);
+app.controller('listcntroller', function ($scope) {
+    var self = this;
+    self.list = [];
+    self.addTask = function () {
+        if ($scope.task == undefined || $scope.task ==" ") {
+            alert('Please enter a task');
+        }
+        else {
+            document.getElementById('list1').value = " ";
+            self.list.push($scope.task);
+            $scope.task = null;            
+        }
+    };
+});
